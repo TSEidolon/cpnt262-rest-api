b5c3703de6edb27620aec6251ad10acaa794c9fa
Edgar Francis Caballero <efrcaballero@gmail.com>
2022-02-23 20:15:45 -0700

    added files

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# cpnt262-rest-api
+# CPNT 262 Assignment 1 - Fetching API Data
+## By: Edgar Caballero
+
+Troubleshooting:
+1. Had trouble referencing a URL for the API since it did not have an object with its own url to it.
+  - I went to the <a href="https://gadhagod.github.io/Hyrule-Compendium-API/#/" target="_blank"> Hyrule compendium API</a> website and realized that there was a way to display certain data by their ID numbers.
+```
+ <a href="https://botw-compendium.herokuapp.com/api/v2/entry/${monsterData.id}" target="_blank">Raw Data</a>
+
+```
+
+2. `addEventListener` did not work due to me referencing the <strong> parent element instead of the child element </strong> that I was targeting.
+  - Fixed when I properly referenced the element
+
+Attributions:
+1. <a href="https://www.youtube.com/watch?v=V_Kr9OSfDeU " target="_blank"> Async await video</a> 
+2. <a href="https://www.youtube.com/watch?v=y17RuWkWdn8" target="_blank"> DOM Manipulation for JavaScript</a> 
+3. <a href="http://www.javascriptkit.com/javatutors/trycatch2.shtml" target="_blank"> Error and throw new error</a> 
+ - Used to define the error and throw new error commands since i was still confused on their differences.
+    - "`throw new Error`" throws your own exeptions to force it to happen on demand instead of waiting for other errors above it to occur.
+    - "`Error`" gets passed into the catch clause when an error event occurs.
+
+4. Tony Grimes for the <a href="https://github.com/sait-wbdv/in-class-w22/blob/main/02-17-exceptions-apis/02-disney-demo-starter/js/app.js" target="_blank"> Disney demo starter code</a> .
---
+++ js/app.js
@@ -0,0 +1,50 @@
+
+// Random Index function
+const randomAPI = (length) => {
+  return Math.floor(Math.random() * length );
+}
+
+// Document Qeury Selectors
+// const monsterName = document.querySelector ('.monsterName');
+// const monsterImagesContainer = document.queryCommandValue ('.monsterImagesContainer');
+// const monsterDescription = document.querySelector ('.monsterDescription');
+// const monsterURL = document.querySelector ('.monsterURL');
+const card = document.querySelector ('.card');
+const submitBTN = document.querySelector ('.submitBTN')
+
+ // API Async Function
+ const BOTWData = async () => {
+  try {
+    url = 'https://botw-compendium.herokuapp.com/api/v2';
+    const response = await fetch (url);
+    console.log (url);
+      if (!response.ok) {
+        throw new Error ("Could not find any monsters in Hyrule" + response.message);
+      }
+    
+    const data = await response.json ();
+    console.log (data.data.monsters);
+
+    const monsterData = data.data.monsters[randomAPI(data.data.monsters.length)];
+    console.log (monsterData);
+
+
+    const output = `
+        <h1>${monsterData.name}</h1>
+        <img src="${monsterData.image}" alt="Image of ${monsterData.name}">
+        <p> ${monsterData.description} </p>
+        <p> <a href="https://botw-compendium.herokuapp.com/api/v2/entry/${monsterData.id}" target="_blank">Click for raw data</a> </p>
+      `;
+    card.innerHTML = output;
+    
+    
+  } catch (err) {
+    console.log ("Monster data error. " + err )
+  }
+  // console.log (BOTWData());
+}
+
+
+
+// Button Event Handler
+submitBTN.addEventListener ('click', BOTWData);
